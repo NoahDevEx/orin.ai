f248c8a24588beafaa0387f0bc69aca9ce102ad5
Noah <noaholuwasegun60@gmail.com>
2026-01-07 13:23:47 +0100

    Fixed error

--- lib/Features/Home/homeobjects.dart
+++ lib/Features/Home/homeobjects.dart
@@ -11,143 +11,114 @@ String song5 = "lib/Data/Assets/Images/song5.png";
 String song6 = "lib/Data/Assets/Images/song6.png";
 String song7 = "lib/Data/Assets/Images/song7.png";
 
-List<Container> swipeCards() 
-{
-  return 
-  [
+List<dynamic> swipeCards() {
+  return [
     Container(
-      
       child: ClipRRect(
         borderRadius: BorderRadius.circular(30.0),
-        child: Image.asset(
-        song7,
-      width: 80.w,
-      height:200.h,
-        
-        ),
+        child: Image.asset(song7, width: 80.w, height: 200.h),
       ),
     ),
 
     SizedBox(
       width: 100.w,
-      height:200.h,
+      height: 200.h,
       child: ClipRRect(
         borderRadius: BorderRadius.circular(30.0),
-        child: Image.asset(
-          song6,
-          fit: BoxFit.cover,
-        ),
+        child: Image.asset(song6, fit: BoxFit.cover),
       ),
     ),
 
     SizedBox(
       width: 140.w,
-      height:200.h,
-     child: ClipRRect(
+      height: 200.h,
+      child: ClipRRect(
         borderRadius: BorderRadius.circular(30.0),
-        child: Image.asset(
-          song5,
-          fit: BoxFit.cover,
-        ),
+        child: Image.asset(song5, fit: BoxFit.cover),
       ),
     ),
 
     SizedBox(
       width: 140.w,
-      height:200.h,
-       child: ClipRRect(
+      height: 200.h,
+      child: ClipRRect(
         borderRadius: BorderRadius.circular(30.0),
-        child: Image.asset(
-          song4,
-          fit: BoxFit.cover,
-        ),
+        child: Image.asset(song4, fit: BoxFit.cover),
       ),
     ),
 
     SizedBox(
       width: 140.w,
-      height:200.h,
-       child: ClipRRect(
+      height: 200.h,
+      child: ClipRRect(
         borderRadius: BorderRadius.circular(30.0),
-        child: Image.asset(
-          song3,
-          fit: BoxFit.cover,
-        ),
+        child: Image.asset(song3, fit: BoxFit.cover),
       ),
     ),
 
     SizedBox(
       width: 140.w,
-      height:200.h,
-       child: ClipRRect(
+      height: 200.h,
+      child: ClipRRect(
         borderRadius: BorderRadius.circular(30.0),
-        child: Image.asset(
-          song2,
-          fit: BoxFit.cover,
-        ),
+        child: Image.asset(song2, fit: BoxFit.cover),
       ),
     ),
 
     SizedBox(
       width: 140.w,
-      height:200.h,
-    child: ClipRRect(
+      height: 200.h,
+      child: ClipRRect(
         borderRadius: BorderRadius.circular(30.0),
-        child: Image.asset(
-          song1,
-          fit: BoxFit.cover,
-        ),
+        child: Image.asset(song1, fit: BoxFit.cover),
       ),
     ),
   ];
 }
 
-
-Widget rowSongCard(String imagePath)
-{
+Widget rowSongCard(String imagePath) {
   return SizedBox(
     width: 150.w,
     height: 150.h,
     child: ClipRRect(
       borderRadius: BorderRadius.circular(10.0),
-      child: Image.asset(
-        imagePath,
-        fit: BoxFit.cover,
-      ),
+      child: Image.asset(imagePath, fit: BoxFit.cover),
     ),
   );
 }
 
-Widget songType(String title)
-{
+Widget songType(String title) {
   return Column(
     children: [
       Container(
-      width: 200.w,
-      height: 60.h,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10.0),
-        color: white.withOpacity(0.3),
+        width: 200.w,
+        height: 60.h,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: white.withOpacity(0.3),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(width: 10.w),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(15.0),
+              child: Image.asset(song1, height: 30.h, width: 30.w),
+            ),
+            SizedBox(width: 10.w),
+            Text(
+              title,
+              style: majorFont.copyWith(color: white, fontSize: 18.0),
+            ),
+          ],
+        ),
       ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          SizedBox(width: 10.w,),
-          ClipRRect(
-          borderRadius: BorderRadius.circular(15.0),
-          child: Image.asset(song1,height:30.h,width:30.w,)),
-          SizedBox(width: 10.w,),
-          Text(title,style: majorFont.copyWith(color: white,fontSize: 18.0),)
-        ],
-      )
-      ),
-      SizedBox(height: 10.h,),
+      SizedBox(height: 10.h),
     ],
   );
 }
 
-List<String> title = 
-[
+List<String> title = [
   "Orin Jùjú",
   "Orin Háiláìfù",
   "Orin Fújì",
